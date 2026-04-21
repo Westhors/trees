b223e88e2f2ccc8030e039f103ee8e54e3fea658
ahmed abdullah <abdallah@pyramidsfreight.com>
2026-04-21 17:31:58 +0200

    delete-account

--- routes/api.php
+++ routes/api.php
@@ -110,7 +110,7 @@ Route::post('login', [MemberController::class, 'login']);
 Route::post('logout', [MemberController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('check-auth', [MemberController::class, 'checkAuth'])->middleware('auth:sanctum');
 Route::post('update-member', [MemberController::class, 'updateMember'])->middleware('auth:sanctum');
-Route::delete('user/delete-account', [MemberController::class, 'deleteAccount'])
+Route::post('delete-account', [MemberController::class, 'deleteAccount'])
     ->middleware('auth:sanctum');
 
 Route::get('members/tree', [MemberController::class,'tree']);
